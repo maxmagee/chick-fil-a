784783127792c174c5d0addbd16e46e7811a7c5d
Max Magee <maxmagee@users.noreply.github.com>
2018-07-17 01:24:24 -0500

    #24 Added a heading font to the global styles.

--- chick-fil-a/src/config/globalStyles.js
+++ chick-fil-a/src/config/globalStyles.js
@@ -1,14 +1,21 @@
 import colors from './colors';
 
 export { colors };
+
 export const navigationHeaderTitle = {
   fontFamily: 'Times New Roman',
   fontSize: 21,
   fontWeight: '200'
 };
 
+export const heading = {
+  fontFamily: 'Times New Roman',
+  fontSize: 16,
+  fontWeight: 'normal'
+};
+
 export const paragraphText = {
   fontFamily: 'Arial',
-  fontSize: 15,
-  fontWeight: 'normal'
+  fontSize: 14,
+  fontWeight: '100'
 };
