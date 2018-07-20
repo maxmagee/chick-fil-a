bb3d38701c4d25775c1f9be3d3c95387d81fe5c4
Max Magee <maxmagee@users.noreply.github.com>
2018-07-20 00:42:45 -0500

    #36 Added image assets to be displayed on the privacy screen.

---
+++ chick-fil-a/assets/privacy/index.js
@@ -0,0 +1,13 @@
+import biometrics from './biometricsBanner.png';
+import email from './emailBanner.png';
+import gps from './gpsBanner.png';
+import password from './passwordBanner.png';
+import payment from './paymentBanner.png';
+import phone from './phoneBanner.png';
+import photos from './photosBanner.png';
+import security from './securityBanner.png';
+import unique from './uniqueBanner.png';
+import user from './userBanner.png';
+import video from './videoBanner.png';
+
+export { biometrics, email, gps, password, payment, phone, photos, security, unique, user, video };
